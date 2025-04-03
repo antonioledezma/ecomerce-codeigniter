12f34c217c9eeed023d6198e9e038945b47440b1
antonio <ledezma.antonio.ignacio@gmail.com>
2025-04-03 17:41:08 -0300

    se borro el router.php... porque viendo la doc, se puede iniciar usando: >> php spark serve

--- app/Views/about.php
+++ app/Views/about.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola desde CodeIgniter</h1>
-    <a href="/about">Ir a about</a>
+    <h1>Hola desde About</h1>
+    <a href="/">Ir a principal</a>
 </body>
 </html>
