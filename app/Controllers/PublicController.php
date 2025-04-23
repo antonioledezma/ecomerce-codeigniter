f3956d24d67d08023b36320f22d3dfba316d54c7
Antonio Ledezma <ledezma.antonio.ignacio@gmail.com>
2025-04-23 11:47:06 -0300

    progress

--- app/Controllers/PublicController.php
+++ app/Controllers/PublicController.php
@@ -17,6 +17,9 @@ class PublicController extends BaseController {
   public function principal(){
     $data = [
       'title' => 'Principal',
+      'breadcrumbs-items' => [
+        ['title' => 'Principal', 'url' => '/principal']
+      ],
       'productList' => $this->productoModel->findAll()
     ];
     echo $this->mustacheRenderer->render('page/principal', $data);
@@ -24,35 +27,55 @@ class PublicController extends BaseController {
 
   public function comercializacion(){
     $data = [
-      'title' => 'Principal'
+      'title' => 'Principal',
+      'breadcrumbs-items' => [
+        ['title' => 'Principal', 'url' => '/principal'],
+        ['title' => 'Comercialización', 'url' => '/comercializacion'],
+      ]
     ];
     echo $this->mustacheRenderer->render('page/comercializacion', $data);
   }
 
   public function informacionDeContacto(){
     $data = [
-      'title' => 'información de contacto'
+      'title' => 'información de contacto',
+      'breadcrumbs-items' => [
+        ['title' => 'Principal', 'url' => '/principal'],
+        ['title' => 'información de contacto', 'url' => '/informacion-de-contacto'],
+      ]
     ];
     echo $this->mustacheRenderer->render('page/informacion-de-contacto', $data);
   }
 
   public function quienesSomos(){
     $data = [
-      'title' => 'Quienes somos'
+      'title' => 'Quienes somos',
+      'breadcrumbs-items' => [
+        ['title' => 'Principal', 'url' => '/principal'],
+        ['title' => 'Quienes somos', 'url' => '/quienes-somos'],
+      ]
     ];
     echo $this->mustacheRenderer->render('page/quienes-somos', $data);
   }
 
   public function terminosYUso(){
     $data = [
-      'title' => 'Términos y uso'
+      'title' => 'Términos y uso',
+      'breadcrumbs-items' => [
+        ['title' => 'Principal', 'url' => '/principal'],
+        ['title' => 'Términos y uso', 'url' => '/terminos-y-uso'],
+      ]
     ];
     echo $this->mustacheRenderer->render('page/terminos-y-uso', $data);
   }
 
   public function productDetail(){
     $data = [
-      'title' => 'Producto'
+      'title' => 'Producto',
+      'breadcrumbs-items' => [
+        ['title' => 'Principal', 'url' => '/principal'],
+        ['title' => 'Producto', 'url' => '/product-detail'],
+      ]
     ];
     echo $this->mustacheRenderer->render('page/product-detail', $data);
   }
